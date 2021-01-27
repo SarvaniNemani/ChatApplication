0c67cdba3202026f08b9a9e78afa2067a5d96a4e
SarvaniNemani <sarvani.nemani@appitventures.com>
2021-01-27 15:21:45 +0530

    added fields in table

--- migrations/20210127124235_create_user.js
+++ migrations/20210127124235_create_user.js
@@ -7,6 +7,7 @@ exports.up = function(knex) {
         table.string('email')
         table.string('phone')
         table.string('username')
+        table.string('full_name')
         table.string('password')
 
         table.timestamp('created_at').defaultTo(knex.fn.now());
